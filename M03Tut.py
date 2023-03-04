34dd482a2664aa342107507ac41cec147bf10045
obonekenob <pthompson87@ivytech.edu>
2023-03-04 11:52:58 -0500

    M03-Tutorial-1

---
+++ M03Tut.py
@@ -0,0 +1,16 @@
+""" 
+    M03 Tutorial
+    Paul R.Thompson
+    File name: M03Tut.py
+"""
+
+# Functional paradigm
+
+def M03Subtract1(x,y):
+    return (x - y)
+
+def M03Division1(x,y):
+    return(x // y)
+
+print(M03Subtract1(20,12))
+print(M03Division1(24,12))
