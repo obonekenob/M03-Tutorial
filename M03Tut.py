c1f64e4fed843c253829bb03460db2e52224366d
obonekenob <pthompson87@ivytech.edu>
2023-03-07 21:54:36 -0500

    M03-Tutorial-3

--- M03Tut.py
+++ M03Tut.py
@@ -4,13 +4,53 @@
     File name: M03Tut.py
 """
 
-# Functional paradigm
+# OOP paradigm Class and methods (class name is do_math)
+class do_math:
 
-def M03Subtract1(x,y):
+    # initialize variables in class
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    # subtract method
+    def subnum(self):
+        return self.x - self.y
+    
+    # floating point division method
+    def divnumf(self):
+        return self.x / self.y
+
+    # integer division method
+    def divnumi(self):
+        return self.x // self.y
+
+# Functional paradigm functions
+# A Subtract function
+def subtract(x, y):
     return (x - y)
 
-def M03Division1(x,y):
+# A Float Division function
+def dividef(x, y):
+    return(x / y)
+
+# An Integer Division function
+def dividei(x, y):
     return(x // y)
 
-print(M03Subtract1(20,12))
-print(M03Division1(24,12))
+# Run the functions and print output
+
+print("\nOutput from functions")
+print(subtract(24, 12))
+print(dividei(24, 12))
+print(dividef(24, 12))
+
+# insantiate the class
+math_instance = do_math(24, 12)
+
+# call the methods and print output
+
+print("\nClass do_math output")
+print(math_instance.subnum())
+print(math_instance.divnumf())
+print(math_instance.divnumi())
+print("\n")
--- READDME.md
+++ READDME.md
@@ -1,3 +1,16 @@
 # M03-Tutorial
 Functional vs OOP Programming  
+
+Program output below.  
+
+
+Output from functions  
+12  
+2  
+2.0  
+  
+Class do_math output  
+12  
+2.0  
+2  
   
